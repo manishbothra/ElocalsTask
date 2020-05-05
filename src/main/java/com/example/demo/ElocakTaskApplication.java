467fb06d7dc163e93520e04988522fb1465f6a34
manishbothra <manishbothra48@gmail.com>
2020-05-05 18:44:40 +0530

    First commit

---
+++ src/main/java/com/example/demo/ElocakTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElocakTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElocakTaskApplication.class, args);
+	}
+
+}
